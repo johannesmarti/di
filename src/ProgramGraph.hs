7c8b6ba4184cfca2897c17053c4538563d2ba6d4
Johannes Marti <johannes.marti@gmail.com>
2024-12-27 22:16:41 +0100

    use one map in Program graph instead of three

--- src/ProgramGraph.hs
+++ src/ProgramGraph.hs
@@ -1,40 +1,45 @@
 module ProgramGraph (
-
+  ProgramGraph,
+  fromTupleList
 ) where
 
 import Control.Exception (assert)
 
-import Data.Set as Set
+import Data.List (nub)
 import Data.Map.Strict as Map
 import Data.Maybe
+import Data.Set as Set
 
 import qualified DatalogProgram as DP
+
 
 data Unfolding r v n = Atom (DP.Atom r v) | Equality v v | Top | Bot |
                        And n n | Or n n | Exists v n | Project v n |
                        Assign v v n
 
-data ProgramGraph r v n = ProgramGraph {
-  unfolding :: Map n (Unfolding r v n),
-  predecessorMap :: Map n (Set n),
-  outputVariablesMap :: Map n (Set v)
+data NodeData r v n = NodeData {
+  unfolding :: Unfolding r v n,
+  predecessors :: Set n,
+  outputVariables :: Set v
 }
 
+data ProgramGraph r v n = ProgramGraph {dataMap :: (Map n (NodeData r v n))}
 
 -- basic accessing functions
 
 domain :: ProgramGraph r v n -> Set n
-domain graph = keysSet (unfolding graph)
+domain (ProgramGraph m) = keysSet m
 
-outputVariables :: Ord n => ProgramGraph r v n -> n -> Set v
-outputVariables graph node = 
+dataOfNode :: Ord n => ProgramGraph r v n -> n -> NodeData r v n
+dataOfNode (ProgramGraph m) node =
   let err = error "node not in ProgramGraph"
-  in fromMaybe err (Map.lookup node (outputVariablesMap graph))
+  in fromMaybe err (Map.lookup node m)
 
-unfold :: Ord n => ProgramGraph r v n -> n -> Unfolding r v n
-unfold graph node =
-  let err = error "node not in ProgramGraph"
-  in fromMaybe err (Map.lookup node (unfolding graph))
+unfoldNode :: Ord n => ProgramGraph r v n -> n -> Unfolding r v n
+unfoldNode graph node = unfolding (dataOfNode graph node)
+
+outputVariablesOfNode :: Ord n => ProgramGraph r v n -> n -> Set v
+outputVariablesOfNode graph node = outputVariables (dataOfNode graph node)
 
 nodesInUnfolding :: Ord n => Unfolding r v n -> Set n
 nodesInUnfolding (And a b) = Set.fromList [a,b]
@@ -44,28 +49,21 @@ nodesInUnfolding (Project v a) = Set.singleton a
 nodesInUnfolding (Assign v w a) = Set.singleton a
 nodesInUnfolding _ = Set.empty
 
-successors :: Ord n => ProgramGraph r v n -> n -> Set n
-successors graph node = nodesInUnfolding (unfold graph node)
+successorsOfNode :: Ord n => ProgramGraph r v n -> n -> Set n
+successorsOfNode graph node = nodesInUnfolding (unfoldNode graph node)
 
-predecessors :: Ord n => ProgramGraph r v n -> n -> Set n
-predecessors graph node =
-  let err = error "node not in ProgramGraph"
-  in fromMaybe err (Map.lookup node (predecessorMap graph))
+predecessorsOfNode :: Ord n => ProgramGraph r v n -> n -> Set n
+predecessorsOfNode graph node = predecessors (dataOfNode graph node)
 
 
 -- checking the consistency of the data
 
-domsAreCoherent :: Ord n => ProgramGraph r v n -> Bool
-domsAreCoherent graph =
-  dom == keysSet (predecessorMap graph) && 
-  dom == keysSet (outputVariablesMap graph) where dom = domain graph
-
 succPredInDom :: Ord n => ProgramGraph r v n -> Bool
 succPredInDom graph =
- all ((`isSubsetOf` dom) . succ) dom &&
- all ((`isSubsetOf` dom) . pred) dom where
-    succ = successors graph
-    pred = predecessors graph
+ all ((`isSubsetOf` dom) . succs) dom &&
+ all ((`isSubsetOf` dom) . preds) dom where
+    succs = successorsOfNode graph
+    preds = predecessorsOfNode graph
     dom = domain graph
 
 converse :: Ord y => Set x -> (x -> Set y) -> (y -> Set x)
@@ -75,9 +73,9 @@ sameOnDom :: (Ord x, Eq y) => Set x -> (x -> y) -> (x -> y) -> Bool
 sameOnDom dom f g = all (\a -> f a == g a) dom
 
 succPredMatch :: Ord n => ProgramGraph r v n -> Bool
-succPredMatch graph = (sameOnDom dom (predecessors graph) pred') where
+succPredMatch graph = (sameOnDom dom (predecessorsOfNode graph) pred') where
   dom = domain graph
-  pred' = converse dom (successors graph)
+  pred' = converse dom (successorsOfNode graph)
 
 outputVariablesFromInputVariables :: Ord v => Unfolding r v n -> Set v -> Set v
 outputVariablesFromInputVariables (Atom at) _ = Set.fromList $ DP.arguments at
@@ -95,12 +93,13 @@ outputVariablesFromInputVariables (Assign v w _) inVars =
 
 outputVariablesCoherent :: (Ord v, Ord n) => ProgramGraph r v n -> Bool
 outputVariablesCoherent graph =
-    all checkVariablesAroundNode (Map.toList (unfolding graph)) where
-  checkVariablesAroundNode (n, uf) = let
-      maybeInputVars = let succsSet = successors graph n
+    all checkVariablesAroundNode (Map.toList (dataMap graph)) where
+  checkVariablesAroundNode (_, NodeData uf _ outVars) = let
+      maybeInputVars = let succsSet = nodesInUnfolding uf
                            someNode = Set.lookupMin succsSet
-                           someInVars = fmap (outputVariables graph) someNode 
-                           allInVars = Prelude.map (outputVariables graph)
+                           someInVars = fmap (outputVariablesOfNode graph)
+                                             someNode 
+                           allInVars = Prelude.map (outputVariablesOfNode graph)
                                                    (Set.toList succsSet)
         in case someInVars of
              Just set -> if (all (== set) allInVars)
@@ -109,16 +108,31 @@ outputVariablesCoherent graph =
              Nothing -> error "we don't have incoming nodes"
                           -- this error will only trigger if there is a
                           -- mistake in outputVariablesFromInputVariables
-      outVars = outputVariables graph n
     in case maybeInputVars of
          Just set -> outputVariablesFromInputVariables uf set == outVars
          Nothing  -> False
 
+-- Should also check that renamed and bound and projected variables make
+-- sense. I am not yet sure how to do this well.
 isCoherent :: (Ord v, Ord n) => ProgramGraph r v n -> Bool
 isCoherent graph =
-  domsAreCoherent graph &&
   succPredInDom graph &&
   succPredMatch graph &&
   outputVariablesCoherent graph
 
--- construction
+-- creation
+isNub :: Eq a => [a] -> Bool
+isNub list = length list == length (nub list)
+
+fromTupleList :: (Ord v, Ord n) => [(n, Unfolding r v n, Set v)]
+                                   -> ProgramGraph r v n
+fromTupleList tupleList = assert (isNub (Prelude.map (\(f,_,_) -> f) tupleList)) $
+  let domSet = Set.fromList (Prelude.map (\(f,_,_) -> f) tupleList)
+      ufList = Prelude.map (\(f,uf,_) -> (f,uf)) tupleList
+      succs n = nodesInUnfolding (fromJust (error "your mum")
+                                          (Prelude.lookup n ufList))
+      preds = converse domSet succs
+      aList = Prelude.map (\(n,u,v) -> (n, NodeData u (preds n) v)) tupleList
+      graph = ProgramGraph (Map.fromList aList)
+  in assert (isCoherent graph) graph
+
